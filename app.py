c5dabbcf4ff12b5c6b83587b5c9c68eda5b26c2f
BaBs-S <65789801+BaBs-S@users.noreply.github.com>
2024-10-06 21:11:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,6 @@ import os
 
 
 app = Flask(__name__)
-# app.config["MYSQL_HOST"] = "localhost"
-# app.config["MYSQL_USER"] = "root"
-# app.config["MYSQL_PASSWORD"] = "root"
-# app.config["MYSQL_DB"] = "espaceexotique"
-# mysql = MySQL(app)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -20,19 +15,7 @@ def index():
 
 
 
-# @app.route('/produits', methods=['POST', 'GET'])
-# def products():
-#     return render_template('produits.html')
-
-# @app.route('/contacts', methods=['POST', 'GET'])
-# def contact():
-#     return render_template('contact.html')
-
-
-# @app.route('/catalogue')
-# def download():
-#     return send_from_directory(directory='static', path='pdf/catalogue.pdf', as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
-    #app.run(host='10.11.96.151', port=5000, debug=True)
+    app.run()
+    
